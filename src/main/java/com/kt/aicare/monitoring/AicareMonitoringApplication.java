c828cb0c8c312d3c722cb022e7e9a8db60ec4947
heewon <yhwcs@naver.com>
2024-03-19 09:19:59 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/kt/aicare/monitoring/AicareMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.kt.aicare.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicareMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicareMonitoringApplication.class, args);
+    }
+
+}
